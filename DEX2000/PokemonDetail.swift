2594ae92e572720d646ebdca62687770e32567cf
simonwilson1 <163290657+simonwilson1@users.noreply.github.com>
2025-02-27 11:42:27 +0000

    Extend Pokemon build stats model and properties

--- DEX2000/PokemonDetail.swift
+++ DEX2000/PokemonDetail.swift
@@ -19,7 +19,7 @@ struct PokemonDetail: View {
         ScrollView {
             
             ZStack {
-                Image(.normalgrasselectricpoisonfairy)
+                Image(pokemon.background)
                     .resizable()
                     .scaledToFit()
                     .shadow(color: .black, radius: 6)
